7a85bf3508922960538bb52c612be4a10a6cf8fe
Armel Dzian <armel.dzian@gmail.com>
2018-05-04 11:31:43 +0200

    added player, gravity, still no collision

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
-
 function init(){
+    var click = false;
     var canvas = document.getElementById("myCanvas"); // html reference for the cenvas
     var ctx = canvas.getContext("2d"); // html reference for the drawing tool
     var obstacles = [];
+    var player = {x:canvas.width/6, y:canvas.height/2 - 16, height:16, width:8}
 
     function getObstacle(randomizer, array){
         if(randomizer < 0.005){
@@ -12,13 +13,13 @@ function init(){
         }
     }
 
-    getObstacle(0,obstacles);
+    getObstacle(0,obstacles); // first obstacle
 
-    
-
-    //acceleration = position change 
+    //speed and acceleration
     var dx = 0.5;
-    var playerYchange = 0;
+    var verticalSpeedPlayer = 0;
+    var gravity = 0.07;
+
 
     function draw() { // the drawing function
         ctx.clearRect(0, 0, canvas.width, canvas.height); // refresh
@@ -31,11 +32,37 @@ function init(){
             ctx.closePath();
         }
 
+        ctx.beginPath(); // player drawing
+        ctx.rect(player.x, player.y, player.width, player.height);
+        ctx.fillStyle = "#cc0000";
+        ctx.fill();
+        ctx.closePath();
+
         ctx.beginPath(); // draws the ground line
             ctx.rect(0, canvas.height/2, canvas.width, 1);
             ctx.fillStyle = "#666666";
             ctx.fill();
         ctx.closePath();
+
+        document.body.onkeyup = function(e){
+            if(e.keyCode == 32){
+                console.log("jump");
+                verticalSpeedPlayer = 3;
+            }
+        }
+
+
+        if (player.y + player.height >= canvas.height/2 && verticalSpeedPlayer < 0){
+            console.log("walk");
+            player.y = canvas.height/2-player.height;
+            verticalSpeedPlayer = 0;
+            verticalAccelerationPlayer = 0;
+            dx = 0.5;
+        }else{
+            player.y -= verticalSpeedPlayer;
+             verticalSpeedPlayer -= gravity; 
+            dx = 1;
+        }
 
         for (var i = 0; i < obstacles.length; i++){ // obstacle position update
             obstacles[i].x -= dx;
@@ -47,7 +74,8 @@ function init(){
                 i--;
             }
         }
-        if (obstacles[obstacles.length-1].x < canvas.width-obstacles[obstacles.length-1].width){
+
+        if (obstacles[obstacles.length-1].x < canvas.width-obstacles[obstacles.length-1].width){ //if the last obstacle is clear of the canvas edge, try to generate  new one
             getObstacle(Math.random(),obstacles);
         }
     }
